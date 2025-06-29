97e06e1bdc0096f9cfa19f62837b629b63495b66
Nha911 <selenagamingti2@gmail.com>
2025-06-29 16:28:55 +0700

    Update project files

--- demo_project/urls.py
+++ demo_project/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', include('my_web_demo.urls')),# This line includes the URLs from the my_web_app app
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    re_path(r'^index\\.html$', RedirectView.as_view(url='/', permanent=True)),
 ]
 
 if settings.DEBUG:
